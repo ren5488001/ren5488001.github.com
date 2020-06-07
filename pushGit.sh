d65119cda88e1fa3c6b72d016f3b96d5deb8d51e
ren5488001 <ren5488001@163.com>
2020-06-07 15:24:37 +0800

    2020-06-07

---
+++ pushGit.sh
@@ -0,0 +1,14 @@
+if [ $# -eq 0 ]
+then
+    pushmessage=`date +%Y-%m-%d`
+else
+    pushmessage="$*"
+fi
+
+#pushmessage=`date +%Y-%m-%d`
+
+echo ${pushmessage}
+
+git add -A
+git commit -m "${pushmessage}"
+git push origin master
